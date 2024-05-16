db98a11e1953a9ff13fb122a2a5789cc97559783
Jeremstym <jeremstym@gmail.com>
2024-05-16 14:42:40 +0200

    Monitor time loading image

--- datasets_processor/ContrastiveImagingAndTabularDataset.py
+++ datasets_processor/ContrastiveImagingAndTabularDataset.py
@@ -10,6 +10,8 @@ from torchvision.transforms import transforms
 from torchvision.io import read_image
 import cv2
 
+from time import time
+
 
 class ContrastiveImagingAndTabularDataset(Dataset):
   """
@@ -116,9 +118,12 @@ class ContrastiveImagingAndTabularDataset(Dataset):
     """
     im = self.data_imaging[index]
     if self.live_loading:
+      start = time()
       im = cv2.imread(im)
       im = im / 255
       im = torch.tensor(im, dtype=torch.float).permute(2,0,1)
+      end = time()
+      print(f'Loaded image in {end-start} seconds')
     ims = [self.transform(im)]
     if random.random() < self.augmentation_rate:
       ims.append(self.transform(im))
