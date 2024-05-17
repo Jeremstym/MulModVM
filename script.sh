9da0bbebf6bc0e959c60ca11c9d8b597f6c04b69
Jeremstym <jeremstym@gmail.com>
2024-05-17 17:28:13 +0200

    Enable one hot when no transformer

--- script.sh
+++ script.sh
@@ -12,6 +12,6 @@ uname -a
 nvidia-smi
 cd MulModVM
 
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=False wandb_name=Bob1-noTransformer
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=False one_hot=True wandb_name=Bob1-noTransformer-OneHot
+# poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
 
