0cce0e7d32988a83e3d9db18ab496055ef9d7c25
Jeremstym <jeremstym@gmail.com>
2024-05-19 15:09:35 +0200

    New script bash using thin node

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=hard
+#SBATCH --nodelist=thin
 #SBATCH --job-name=BoBW-attention
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
@@ -12,6 +13,6 @@ uname -a
 nvidia-smi
 cd MulModVM
 
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=False one_hot=True wandb_name=Bob1-noTransformer-OneHot
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
 
