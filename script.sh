28377171ecaf4076b8b46c4a1ec5dba6edbb36ec
Jeremstym <jeremstym@gmail.com>
2024-05-20 16:28:25 +0200

    Set poetry run header

--- script.sh
+++ script.sh
@@ -14,4 +14,4 @@ cd MulModVM
 
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
-python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
