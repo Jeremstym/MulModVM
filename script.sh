703ebd80e2ffb3e838d78b04a904f37a7da9e8b9
Jeremstym <jeremstym@gmail.com>
2024-05-20 16:27:28 +0200

    New script for finetuning Bob

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --partition=hard
-#SBATCH --nodelist=thin
 #SBATCH --job-name=BoBW-attention
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
@@ -13,6 +12,6 @@ uname -a
 nvidia-smi
 cd MulModVM
 
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
+# poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
-
+python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
