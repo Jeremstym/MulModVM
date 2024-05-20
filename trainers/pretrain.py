96af16711e574592cd37b90615fd814275ce45d9
Jeremstym <jeremstym@gmail.com>
2024-05-20 17:31:16 +0200

    Add label usage option

--- trainers/pretrain.py
+++ trainers/pretrain.py
@@ -34,11 +34,13 @@ def load_datasets(hparams):
     train_dataset = ContrastiveImagingAndTabularDataset(
       hparams.data_train_imaging, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
       hparams.data_train_tabular, hparams.corruption_rate, hparams.field_lengths_tabular, hparams.one_hot,
-      hparams.labels_train, hparams.img_size, hparams.live_loading, missing_values=hparams.missing_values, use_cache=hparams.use_cache, use_transformer=hparams.use_transformer)
+      hparams.labels_train, hparams.img_size, hparams.live_loading, missing_values=hparams.missing_values,
+      use_cache=hparams.use_cache, use_transformer=hparams.use_transformer, use_labels=hparams.use_labels)
     val_dataset = ContrastiveImagingAndTabularDataset(
       hparams.data_val_imaging, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
       hparams.data_val_tabular, hparams.corruption_rate, hparams.field_lengths_tabular, hparams.one_hot,
-      hparams.labels_val, hparams.img_size, hparams.live_loading, use_cache=hparams.use_cache)
+      hparams.labels_val, hparams.img_size, hparams.live_loading,
+      use_cache=hparams.use_cache, use_transformer=hparams.use_transformer, use_labels=hparams.use_labels)
     hparams.input_size = train_dataset.get_input_size()
   elif hparams.datatype == 'imaging':
     transform = grab_image_augmentations(hparams.img_size, hparams.target, hparams.crop_scale_lower)
