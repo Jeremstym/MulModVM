73ae8f179e5b43c13dd6a85c11812b3e79f406b2
Jeremstym <jeremstym@gmail.com>
2024-05-21 12:13:07 +0200

    Modify script bash

--- script.sh
+++ script.sh
@@ -14,5 +14,5 @@ nvidia-smi
 cd MulModVM
 
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=22 use_cache=False use_transformer=False wandb_name=BoB-Baseline1-TrueDS
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=26 use_cache=False use_transformer=False one_hot=True wandb_name=BoB-Baseline1-TrueDS
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
