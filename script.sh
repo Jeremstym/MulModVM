1756d88105bf8557de88a9327cb6d198cb8531a1
Jeremstym <jeremstym@gmail.com>
2024-05-21 11:40:11 +0200

    Prepare script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
-#SBATCH --job-name=BoBW-attention
+#SBATCH --nodelist=led
+#SBATCH --job-name=BoBW-Baseline
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=1-16:00:00
@@ -13,5 +14,5 @@ nvidia-smi
 cd MulModVM
 
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
-# poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True wandb_name=Bob1-Transformer
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=22 use_cache=False use_transformer=False wandb_name=BoB-Baseline1-TrueDS
+# poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
