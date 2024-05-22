b29a9fc9926d75a66ca832f112336e438768c720
Jeremstym <jeremstym@gmail.com>
2024-05-22 15:57:28 +0200

    Simplify training condition to get faster

--- script.sh
+++ script.sh
@@ -14,5 +14,5 @@ nvidia-smi
 cd MulModVM
 
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=128 num_workers=28 use_cache=False use_transformer=True one_hot=False wandb_name=Bob2-Transformer
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=500 pretrain=True batch_size=512 num_workers=4 use_cache=False use_transformer=False one_hot=True wandb_name=BoB-Baseline
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal max_epochs=100 pretrain=True batch_size=256 num_workers=4 persistent_workers=True use_cache=False use_transformer=False one_hot=True wandb_name=BoB-Baseline
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=multimodal checkpoint=/home/stympopper/MulModVM/runs/multimodal/Bob2-Transformer/checkpoint_last_epoch_99.ckpt max_epochs=100 batch_size=128 num_workers=26 wandb_name=BobTransFinetune1 use_cache=False use_transformer=True
