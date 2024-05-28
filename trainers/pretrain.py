955ca3f7ad77448e35967ed34a2ce0bf02126007
Jeremstym <jeremstym@gmail.com>
2024-05-28 15:19:57 +0200

    Important embeddings instead of images

--- trainers/pretrain.py
+++ trainers/pretrain.py
@@ -32,12 +32,12 @@ def load_datasets(hparams):
     hparams.transform = transform.__repr__()
     assert (hparams.one_hot != hparams.use_transformer), 'Cannot use one hot encoding and transformer at the same time'
     train_dataset = ContrastiveImagingAndTabularDataset(
-      hparams.data_train_imaging, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
+      hparams.data_train_imaging_tensors, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
       hparams.data_train_tabular, hparams.corruption_rate, hparams.field_lengths_tabular, hparams.one_hot,
       hparams.labels_train, hparams.img_size, hparams.live_loading, missing_values=hparams.missing_values,
       use_cache=hparams.use_cache, use_transformer=hparams.use_transformer, use_labels=hparams.use_labels, use_embds=hparams.use_embds)
     val_dataset = ContrastiveImagingAndTabularDataset(
-      hparams.data_val_eval_imaging, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
+      hparams.data_val_imaging_tensors, hparams.delete_segmentation, transform, hparams.augmentation_rate, 
       hparams.data_val_tabular, hparams.corruption_rate, hparams.field_lengths_tabular, hparams.one_hot,
       hparams.labels_val, hparams.img_size, hparams.live_loading,
       use_cache=hparams.use_cache, use_transformer=hparams.use_transformer, use_labels=hparams.use_labels, use_embds=hparams.use_embds)
