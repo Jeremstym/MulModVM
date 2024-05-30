0a73b426e07ac2dd620da4d4480db3b2b8e7c8a0
Jeremstym <jeremstym@gmail.com>
2024-05-30 15:34:00 +0200

    Set embd dim to 2048 when training MLP

--- models/SCARF.py
+++ models/SCARF.py
@@ -19,7 +19,10 @@ class SCARF(Pretraining):
     nclasses = hparams.batch_size*2-1
 
     self.initialize_classifier_and_metrics(nclasses, nclasses)
-    self.pooled_dim = self.hparams.tabular_embedding_dim
+    if self.hparams.use_transformer:
+      self.pooled_dim = self.hparams.tabular_embedding_dim
+    else:
+      self.pooled_dim = self.hparams.embedding_dim
 
     print(self.encoder_tabular)
     print(self.projector_tabular)
