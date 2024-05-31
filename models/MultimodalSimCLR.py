c454b4b571f1c90c76b8a17367f37d16598d3dd2
Jeremstym <jeremstym@gmail.com>
2024-05-31 15:51:25 +0200

    Set hparams as attribute

--- models/MultimodalSimCLR.py
+++ models/MultimodalSimCLR.py
@@ -64,7 +64,7 @@ class MultimodalSimCLR(Pretraining):
     
     # Augmented views
     z0, embeddings = self.forward_imaging(im_views[1]) 
-    if hparams.use_transformer:
+    if self.hparams.use_transformer:
       z1, _ = self.forward_tabular(*tab_views)
     else:
       z1, _ = self.forward_tabular(tab_views[1])
