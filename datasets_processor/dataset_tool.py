81e059e335d1ed741ee75ea7a3b54691261b5fc7
Jeremstym <jeremstym@gmail.com>
2024-06-06 01:18:16 +0200

    Use torch to load labels

--- datasets_processor/dataset_tool.py
+++ datasets_processor/dataset_tool.py
@@ -255,7 +255,7 @@ def open_pickle(source_file, *, max_images: Optional[int], label_path: Optional[
         print(f'Loading labels from {label_path}')
         labels = {}
         with open(label_path, 'rb') as file:
-            labels = json.load(file)
+            labels = torch.load(file)
             if labels is not None:
                 labels = { x[0]: x[1] for x in labels }
             else:
