01416f6496aa56038d56f41a41898da10812a2ee
Jeremstym <jeremstym@gmail.com>
2024-06-06 00:54:23 +0200

    Try with usual dataset

--- trainers/evaluate.py
+++ trainers/evaluate.py
@@ -16,20 +16,28 @@ from utils.utils import grab_arg_from_checkpoint, grab_hard_eval_image_augmentat
 
 def load_datasets(hparams):
   if hparams.datatype == 'imaging' or hparams.datatype == 'multimodal':
-    train_dataset = ImageFastDataset(
-      data_path = hparams.data_fast_train_imaging,
-      name = "imaging_only",
-      use_labels = True,
-      delete_segmentation = hparams.delete_segmentation,
-      labels_path_short = hparams.labels_train, 
+    train_dataset = ImageDataset(
+      data_path=hparams.data_train_eval_imaging,
+      labels=hparams.labels_train_eval_imaging,
+      delete_segmentation=hparams.delete_segmentation,
+      eval_train_augment_rate=hparams.augmentation_rate,
+      img_size=hparams.img_size,
+      target=hparams.target,
+      train=True,
+      live_loading=hparams.live_loading,
+      task=hparams.task
       )
-    val_dataset = ImageFastDataset(
-      data_path=hparams.data_fast_val_imaging,
-      name = "imaging_only",
-      use_labels = True,
-      delete_segmentation = hparams.delete_segmentation,
-      labels_path_short = hparams.labels_val,
-      )
+    val_dataset = ImageDataset(
+      data_path=hparams.data_val_eval_imaging,
+      labels=hparams.labels_val_eval_imaging,
+      delete_segmentation=hparams.delete_segmentation,
+      eval_train_augment_rate=0,
+      img_size=hparams.img_size,
+      target=hparams.target,
+      train=False,
+      live_loading=hparams.live_loading,
+      task=hparams.task
+    )
   elif hparams.datatype == 'tabular':
     train_dataset = TabularDataset(hparams.data_train_eval_tabular, hparams.labels_train_eval_tabular, hparams.eval_one_hot, hparams.field_lengths_tabular)
     val_dataset = TabularDataset(hparams.data_val_eval_tabular, hparams.labels_val_eval_tabular, hparams.eval_one_hot, hparams.field_lengths_tabular)
