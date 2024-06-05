d24190bcd514cc1956375441e71c5e929e073079
Jeremstym <jeremstym@gmail.com>
2024-06-06 01:32:08 +0200

    Separate datasets processor when going file

--- script_processing.sh
+++ script_processing.sh
@@ -10,7 +10,9 @@
 
 uname -a
 nvidia-smi
-cd MulModVM/datasets_processor
+cd MulModVM
+
+cd datasets_processor
 
 poetry run python dataset_tool.py --source=/home/stympopper/data/DVMdata/features/train_paths_all_views.pt --dest=/home/stympopper/data/DVMdata/readyTrainLabeled --resolution=128x128 --normalize=True --labels=/home/stympopper/data/DVMdata/features/dataset_train.json
 poetry run python dataset_tool.py --source=/home/stympopper/data/DVMdata/features/val_paths_all_views.pt --dest=/home/stympopper/data/DVMdata/readyValLabeled --resolution=128x128 --normalize=True --labels=/home/stympopper/data/DVMdata/features/dataset_val.json
