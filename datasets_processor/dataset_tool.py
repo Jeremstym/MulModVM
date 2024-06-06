1d61de659003b8603b7f823fc164e99c953e5535
Jeremstym <jeremstym@gmail.com>
2024-06-06 11:10:49 +0200

    Get key from labels

--- datasets_processor/dataset_tool.py
+++ datasets_processor/dataset_tool.py
@@ -255,7 +255,7 @@ def open_pickle(source_file, *, max_images: Optional[int], label_path: Optional[
         print(f'Loading labels from {label_path}')
         labels = {}
         with open(label_path, 'r') as file:
-            labels = json.load(file)
+            labels = json.load(file)["labels"]
             if labels is not None:
                 labels = { x[0]: x[1] for x in labels }
             else:
