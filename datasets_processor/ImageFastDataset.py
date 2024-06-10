89381740cdf388ea4c2c8ecfbcba281b5c774c02
Jeremstym <jeremstym@gmail.com>
2024-06-11 01:16:57 +0200

    Treat image as tensor and not np array

--- datasets_processor/ImageFastDataset.py
+++ datasets_processor/ImageFastDataset.py
@@ -290,8 +290,8 @@ class ImageFastDataset(Dataset):
 
     def __getitem__(self, idx):
         image = self._load_raw_image(self._raw_idx[idx])
-        assert isinstance(image, np.ndarray)
+        assert isinstance(image, torch.Tensor)
         assert list(image.shape) == self.image_shape
-        assert image.dtype == np.uint8
-        image = image.astype(np.float32)
+        assert image.dtype == torch.uint8
+        image = image.astype(torch.float32)
         return image.copy(), self.get_label(idx)
