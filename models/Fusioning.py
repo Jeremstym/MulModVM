eb6d1fdc0429eefca486c6934e55ced4b6a4d6d0
Jeremstym <jeremstym@gmail.com>
2024-06-11 13:31:18 +0200

    Distinguish cases mlp or transformer in forward process

--- models/Fusioning.py
+++ models/Fusioning.py
@@ -112,8 +112,11 @@ class Fusion(pl.LightningModule):
     def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
         x_im = self.encode_imaging(x[0])  # only keep the encoder output
         x_proj_im = self.im_head(x_im)
-        x_tokens_tab = self.tokenize_tabular(x[1])
-        x_tab = self.encode_tabular(x_tokens_tab).squeeze()
+        if self.hparams.tabular_model == "transformer":
+            x_tokens_tab = self.tokenize_tabular(x[1])
+            x_tab = self.encode_tabular(x_tokens_tab).squeeze()
+        else:
+            x_tab = self.encode_tabular(x[1])
         x_proj_tab = self.tab_head(x_tab)
         x = torch.cat([x_proj_im, x_proj_tab], dim=1)
         x = self.head(x)
