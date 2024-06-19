2b5081a11ee3c7ec967c419b78660f125001d6b5
Jeremstym <jeremstym@gmail.com>
2024-06-19 14:54:09 +0200

    Double encoder on condition

--- models/TabularEncoder.py
+++ models/TabularEncoder.py
@@ -24,7 +24,7 @@ class TabularEncoder(nn.Module):
       if 'encoder_tabular.encoder.1.running_mean' in state_dict.keys():
         encoder_name = 'encoder_tabular.encoder.'
         self.encoder = self.build_encoder(original_args)
-      elif 'model.encoder.1.running_mean' in state_dict.keys():
+      elif 'model.encoder.encoder.1.running_mean' in state_dict.keys():
         encoder_name = 'model.encoder.encoder.'
         self.encoder = self.build_encoder(original_args)
       elif 'encoder_projector_tabular.encoder.2.running_mean' in state_dict.keys():
