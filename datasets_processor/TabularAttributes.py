2a54a3d3a8bb1863e1ee8aba91a8a6aa8cfd7aa4
Jeremstym <jeremstym@gmail.com>
2024-06-20 16:12:03 +0200

    Add non physical feature list

--- datasets_processor/TabularAttributes.py
+++ datasets_processor/TabularAttributes.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 CAT_FEATURES_WITH_LABEL = [
-  'Color',
+  'Color', 
   'Bodytype',
   'Gearbox',
   'Fuel_type',
@@ -34,13 +34,25 @@ NUM_FEATURES = [
   'Entry_price', 
   'Engine_size',]
 
-FEATURES =  NUM_FEATURES + CAT_FEATURES
+NUM_FEATURS_NON_PHYSICAL = [
+  'Adv_year',
+  'Adv_month',
+  'Reg_year',
+  'Runned_Miles',
+  'Price',
+  'Seat_num',
+  'Door_num',
+  'Entry_price', 
+  'Engine_size',]
 
-def check_categorical_data(df: pd.DataFrame):
+FEATURES =  NUM_FEATURES + CAT_FEATURES
+FEATURES_NON_PHYSICAL = NUM_FEATURS_NON_PHYSICAL + CAT_FEATURES
+
+def check_categorical_data(df: pd.DataFrame, features: list = FEATURES):
   """
   Checks if the data is categorical or not
   """
-  categorizing = [c in CAT_FEATURES for c in FEATURES]
+  categorizing = [c in CAT_FEATURES for c in features]
   typing = [df[c].dtype == np.dtype('int64') for c in df.columns]
   assert all([c == t for c,t in zip(categorizing, typing)]), "Mismatch between categorical and numerical data"
   return categorizing
