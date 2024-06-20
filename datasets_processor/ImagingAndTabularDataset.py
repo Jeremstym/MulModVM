5b8ffc8a272d6941a417a6812e05bc8eb0719096
Jeremstym <jeremstym@gmail.com>
2024-06-20 16:34:40 +0200

    Add non physical possibility

--- datasets_processor/ImagingAndTabularDataset.py
+++ datasets_processor/ImagingAndTabularDataset.py
@@ -17,25 +17,39 @@ from .TabularAttributes import (
     CAT_FEATURES,
     NUM_FEATURES,
     CAT_FEATURES_WITH_LABEL,
+    NUM_FEATURS_NON_PHYSICAL,
 )
 
-class ImagingAndTabularDataset(Dataset):
-  """
-  Multimodal dataset that imaging and tabular data for evaluation.
 
-  The imaging view has {eval_train_augment_rate} chance of being augmented.
-  The tabular view is never augmented.
-  """
-  def __init__(
-      self,
-      data_path_imaging: str, delete_segmentation: bool, eval_train_augment_rate: float, 
-      data_path_tabular: str, field_lengths_tabular: str, eval_one_hot: bool,
-      labels_path: str, img_size: int, live_loading: bool, train: bool, target: str, tabular_model: str) -> None:
-      
-    # Imaging
-    self.data_imaging_dataset = ImageFastDataset(
+class ImagingAndTabularDataset(Dataset):
+    """
+    Multimodal dataset that imaging and tabular data for evaluation.
+
+    The imaging view has {eval_train_augment_rate} chance of being augmented.
+    The tabular view is never augmented.
+    """
+
+    def __init__(
+        self,
+        data_path_imaging: str,
+        delete_segmentation: bool,
+        eval_train_augment_rate: float,
+        data_path_tabular: str,
+        field_lengths_tabular: str,
+        eval_one_hot: bool,
+        labels_path: str,
+        img_size: int,
+        live_loading: bool,
+        train: bool,
+        target: str,
+        tabular_model: str,
+        use_physical: bool = True,
+    ) -> None:
+
+        # Imaging
+        self.data_imaging_dataset = ImageFastDataset(
             data_path=data_path_imaging,
-            name="imaging", 
+            name="imaging",
             img_size=img_size,
             target=target,
             train_augment_rate=eval_train_augment_rate,
@@ -45,110 +59,125 @@ class ImagingAndTabularDataset(Dataset):
             use_labels=True,
             max_size=None,
         )
-    # self.data_imaging = torch.load(data_path_imaging)
-    self.delete_segmentation = delete_segmentation
-    self.eval_train_augment_rate = eval_train_augment_rate
-    self.live_loading = live_loading
+        # self.data_imaging = torch.load(data_path_imaging)
+        self.delete_segmentation = delete_segmentation
+        self.eval_train_augment_rate = eval_train_augment_rate
+        self.live_loading = live_loading
 
-    # # if self.delete_segmentation:
-    #   for im in self.data_imaging:
-    #     im[0,:,:] = 0
+        # # if self.delete_segmentation:
+        #   for im in self.data_imaging:
+        #     im[0,:,:] = 0
 
-    self.transform_train = grab_hard_eval_image_augmentations(img_size, target)
+        self.transform_train = grab_hard_eval_image_augmentations(img_size, target)
 
-    self.default_transform = transforms.Compose([
-      transforms.Resize(size=(img_size,img_size), antialias=False),
-      transforms.Lambda(lambda x : x.float())
-    ])
+        self.default_transform = transforms.Compose(
+            [
+                transforms.Resize(size=(img_size, img_size), antialias=False),
+                transforms.Lambda(lambda x: x.float()),
+            ]
+        )
 
-    # Tabular
-    use_header = True if tabular_model == "transformer" else False
-    self.field_lengths_tabular = torch.load(field_lengths_tabular)
-    self.eval_one_hot = eval_one_hot
-    self.data_tabular = self.read_and_parse_csv(data_path_tabular, use_header=use_header)
-    
-    # Classifier
-    self.labels = torch.load(labels_path)
+        # Tabular
+        use_header = True if tabular_model == "transformer" else False
+        self.field_lengths_tabular = torch.load(field_lengths_tabular)
+        self.eval_one_hot = eval_one_hot
+        self.use_physical = use_physical
+        self.data_tabular = self.read_and_parse_csv(
+            data_path_tabular, use_header=use_header
+        )
 
-    self.train = train
-  
-  # def read_and_parse_csv(self, path_tabular: str) -> List[List[float]]:
-  #   """
-  #   Does what it says on the box.
-  #   """
-  #   with open(path_tabular,'r') as f:
-  #     reader = csv.reader(f)
-  #     data = []
-  #     for r in reader:
-  #       r2 = [float(r1) for r1 in r]
-  #       data.append(r2)
-  #   return data
+        # Classifier
+        self.labels = torch.load(labels_path)
 
-  def read_and_parse_csv(
-      self,
-      path_tabular: str,
-      missing_values: list = [],
-      use_header: bool = False,
-      max_size: int = None,
-  ) -> List[List[float]]:
-      """
-      Does what it says on the box.
-      """
-      #TODO: add an additional token when using label as a feature
-      # if use_header and self._use_labels:
-      #     FEATURES = NUM_FEATURES + CAT_FEATURES_WITH_LABEL
-      if use_header:
-          FEATURES = NUM_FEATURES + CAT_FEATURES
-          df = pd.read_csv(path_tabular, names=FEATURES)
-          df.drop(missing_values, axis=0, inplace=True)
-          cat_mask = check_categorical_data(df)
-          self.cat_mask = cat_mask
-          field_lengths_tensor = torch.as_tensor(self.field_lengths_tabular)
-          self.cat_card = field_lengths_tensor[cat_mask]
-          data = df.values.tolist()
-      else:
-          with open(path_tabular, "r") as f:
-              reader = csv.reader(f)
-              data = []
-              if max_size is not None:
-                  for idx, r in enumerate(reader):
-                      if idx in missing_values:
-                          continue
-                      r2 = [float(r1) for r1 in r]
-                      data.append(r2)
-                      if idx >= max_size:
-                          break
-              else:
-                  for idx, r in enumerate(reader):
-                      if idx in missing_values:
-                          continue
-                      r2 = [float(r1) for r1 in r]
-                      data.append(r2)
-      return data
+        self.train = train
 
-  def get_input_size(self) -> int:
-    """
-    Returns the number of fields in the table. 
-    Used to set the input number of nodes in the MLP
-    """
-    if self.eval_one_hot:
-      return int(sum(self.field_lengths_tabular))
-    else:
-      return len(self.data_tabular[0])
+    # def read_and_parse_csv(self, path_tabular: str) -> List[List[float]]:
+    #   """
+    #   Does what it says on the box.
+    #   """
+    #   with open(path_tabular,'r') as f:
+    #     reader = csv.reader(f)
+    #     data = []
+    #     for r in reader:
+    #       r2 = [float(r1) for r1 in r]
+    #       data.append(r2)
+    #   return data
 
-  def one_hot_encode(self, subject: torch.Tensor) -> torch.Tensor:
-    """
-    One-hot encodes a subject's features
-    """
-    out = []
-    for i in range(len(subject)):
-      if self.field_lengths_tabular[i] == 1:
-        out.append(subject[i].unsqueeze(0))
-      else:
-        out.append(torch.nn.functional.one_hot(torch.clamp(subject[i],min=0,max=self.field_lengths_tabular[i]-1).long(), num_classes=int(self.field_lengths_tabular[i])))
-    return torch.cat(out)
+    def read_and_parse_csv(
+        self,
+        path_tabular: str,
+        missing_values: list = [],
+        use_header: bool = False,
+        max_size: int = None,
+    ) -> List[List[float]]:
+        """
+        Does what it says on the box.
+        """
+        # TODO: add an additional token when using label as a feature
+        # if use_header and self._use_labels:
+        #     FEATURES = NUM_FEATURES + CAT_FEATURES_WITH_LABEL
+        if use_header:
+            if self.use_physical:
+              FEATURES = NUM_FEATURES + CAT_FEATURES
+            else:
+              FEATURES = NUM_FEATURS_NON_PHYSICAL + CAT_FEATURES
+            df = pd.read_csv(path_tabular, names=FEATURES)
+            df.drop(missing_values, axis=0, inplace=True)
+            cat_mask = check_categorical_data(df, features=FEATURES)
+            self.cat_mask = cat_mask
+            field_lengths_tensor = torch.as_tensor(self.field_lengths_tabular)
+            self.cat_card = field_lengths_tensor[cat_mask]
+            data = df.values.tolist()
+        else:
+            with open(path_tabular, "r") as f:
+                reader = csv.reader(f)
+                data = []
+                if max_size is not None:
+                    for idx, r in enumerate(reader):
+                        if idx in missing_values:
+                            continue
+                        r2 = [float(r1) for r1 in r]
+                        data.append(r2)
+                        if idx >= max_size:
+                            break
+                else:
+                    for idx, r in enumerate(reader):
+                        if idx in missing_values:
+                            continue
+                        r2 = [float(r1) for r1 in r]
+                        data.append(r2)
+        return data
 
-  def get_label(self, index: int) -> int:
+    def get_input_size(self) -> int:
+        """
+        Returns the number of fields in the table.
+        Used to set the input number of nodes in the MLP
+        """
+        if self.eval_one_hot:
+            return int(sum(self.field_lengths_tabular))
+        else:
+            return len(self.data_tabular[0])
+
+    def one_hot_encode(self, subject: torch.Tensor) -> torch.Tensor:
+        """
+        One-hot encodes a subject's features
+        """
+        out = []
+        for i in range(len(subject)):
+            if self.field_lengths_tabular[i] == 1:
+                out.append(subject[i].unsqueeze(0))
+            else:
+                out.append(
+                    torch.nn.functional.one_hot(
+                        torch.clamp(
+                            subject[i], min=0, max=self.field_lengths_tabular[i] - 1
+                        ).long(),
+                        num_classes=int(self.field_lengths_tabular[i]),
+                    )
+                )
+        return torch.cat(out)
+
+    def get_label(self, index: int) -> int:
         """
         Returns the label of the subject at index
         """
@@ -156,45 +185,47 @@ class ImagingAndTabularDataset(Dataset):
         assert label == self.labels[index], f"{label} != {self.labels[index]}"
         return torch.as_tensor(label, dtype=torch.long)
 
-  def get_cat_mask(self) -> torch.Tensor:
+    def get_cat_mask(self) -> torch.Tensor:
         """
         Returns the categorical mask
         """
         return torch.as_tensor(self.cat_mask)
 
-  def get_cat_card(self) -> torch.Tensor:
-      """
-      Returns the categorical cardinalities
-      """
-      return torch.as_tensor(self.cat_card)
+    def get_cat_card(self) -> torch.Tensor:
+        """
+        Returns the categorical cardinalities
+        """
+        return torch.as_tensor(self.cat_card)
 
-  def get_number_of_numerical_features(self) -> int:
-      """
-      Returns the number of numerical features
-      """
-      return len(NUM_FEATURES)
-  
-  def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], List[torch.Tensor], torch.Tensor, torch.Tensor]:
-    # im = self.data_imaging[index]
-    # if self.live_loading:
-    #   im = read_image(im)
-    #   im = im / 255
+    def get_number_of_numerical_features(self) -> int:
+        """
+        Returns the number of numerical features
+        """
+        return len(NUM_FEATURES)
 
-    # if self.train and (random.random() <= self.eval_train_augment_rate):
-    #   im = self.transform_train(im)
-    # else:
-    #   im = self.default_transform(im)
-   
-    im = self.data_imaging_dataset.get_image_from_idx(index)
+    def __getitem__(
+        self, index: int
+    ) -> Tuple[List[torch.Tensor], List[torch.Tensor], torch.Tensor, torch.Tensor]:
+        # im = self.data_imaging[index]
+        # if self.live_loading:
+        #   im = read_image(im)
+        #   im = im / 255
 
-    if self.eval_one_hot:
-      tab = self.one_hot_encode(torch.tensor(self.data_tabular[index]))
-    else:
-      tab = torch.as_tensor(self.data_tabular[index], dtype=torch.float)
+        # if self.train and (random.random() <= self.eval_train_augment_rate):
+        #   im = self.transform_train(im)
+        # else:
+        #   im = self.default_transform(im)
 
-    label = self.get_label(index)
+        im = self.data_imaging_dataset.get_image_from_idx(index)
 
-    return (im, tab), label
+        if self.eval_one_hot:
+            tab = self.one_hot_encode(torch.tensor(self.data_tabular[index]))
+        else:
+            tab = torch.as_tensor(self.data_tabular[index], dtype=torch.float)
 
-  def __len__(self) -> int:
-    return len(self.data_tabular)
+        label = self.get_label(index)
+
+        return (im, tab), label
+
+    def __len__(self) -> int:
+        return len(self.data_tabular)
