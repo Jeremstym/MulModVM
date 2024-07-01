af30cd07766044a90c7596d760776f78e0c1fd10
Jeremstym <jeremstym@gmail.com>
2024-07-01 16:34:00 +0200

    Undo double CLS

--- models/FusionCore.py
+++ models/FusionCore.py
@@ -60,7 +60,7 @@ class FusionCoreCrossAtt(nn.Module):
         """
         Forward pass for the fusion core.
         """
-        tabular = self.cls_token(tabular)
+        # tabular = self.cls_token(tabular) | Already done in TabularTransformer
         tabular = self.positional_encoding_tabular(tabular) # is this necessary?
         image = self.positional_encoding_image(image)
         fusion = self.fusion(image, tabular)
