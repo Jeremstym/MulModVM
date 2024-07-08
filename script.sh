199ddfd77aee375ae5aaf5917197c667f6efc273
Jeremstym <jeremstym@gmail.com>
2024-07-08 18:49:44 +0200

    Put on wait list

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
 #SBATCH --job-name=ResCross2048
-#SBATCH --nodelist=aerosmith
+#SBATCH --nodelist=zz
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
