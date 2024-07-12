1b29c62feed3b8f43e6747cfc25ac1f77a6b89ed
Jeremstym <jeremstym@gmail.com>
2024-07-12 14:03:06 +0200

    Accurate len when nonphysical

--- datasets_processor/ImagingAndTabularDataset.py
+++ datasets_processor/ImagingAndTabularDataset.py
@@ -202,8 +202,10 @@ class ImagingAndTabularDataset(Dataset):
         """
         Returns the number of numerical features
         """
-        return len(NUM_FEATURES)
-
+        if self.use_physical:
+            return len(NUM_FEATURES)
+        else:
+            return len(NUM_FEATURS_NON_PHYSICAL)
     def __getitem__(
         self, index: int
     ) -> Tuple[List[torch.Tensor], List[torch.Tensor], torch.Tensor, torch.Tensor]:
