f777c8f61ea7b9c692c7474a3f964293cd2887c2
Jeremstym <jeremstym@gmail.com>
2024-07-13 18:03:22 +0200

    Change gpu

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
 #SBATCH --job-name=ResCross-512
-#SBATCH --nodelist=zz
+#SBATCH --nodelist=top
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
