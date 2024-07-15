f1db47cd070d187da8f4cbef3aa5c2b1ae5e8f88
Jeremstym <jeremstym@gmail.com>
2024-07-15 14:30:11 +0200

    Script pretrained model

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
 #SBATCH --job-name=ResCross-192-Frozen-Prenorm
-#SBATCH --nodelist=zz
+#SBATCH --nodelist=top
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
@@ -27,4 +27,4 @@ cd MulModVM
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=imaging_and_tabular fusion=True cross_fusion=True image_tokenization=True max_epochs=100 batch_size=256 num_workers=8 wandb_name=CrossAtt-Img768
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=imaging_and_tabular fusion=True max_epochs=100 batch_size=256 num_workers=8 resnet_tokenization=True cross_fusion=True wandb_name=ResNet-CrossAtt-2048_dropout
 # poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=imaging_and_tabular fusion=True max_epochs=100 batch_size=512 num_workers=8 tabular_model=mlp tabular_embedding_dim=2048 use_physical=False wandb_name=BoBConcat-NonPhysical-MLP
-poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=imaging_and_tabular fusion=True max_epochs=100 cross_fusion=True resnet_tokenization=True batch_size=256 num_workers=8 fusion_core.first_prenormalization=True wandb_name=ResCross-192-Frozen-Prenorm4
+poetry run python run.py data_base=/home/stympopper/data/DVMdata/features/ datatype=imaging_and_tabular fusion=True max_epochs=100 cross_fusion=True resnet_tokenization=True batch_size=256 num_workers=8 fusion_core.first_prenormalization=True wandb_name=ResCross-192-Frozen-Pretrained-Prenorm
