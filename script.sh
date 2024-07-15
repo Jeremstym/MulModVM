d43ab3ffe0f9593c04ba8c163b4697bf3bba1180
Jeremstym <jeremstym@gmail.com>
2024-07-15 14:33:22 +0200

    Change gpu

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
 #SBATCH --job-name=ResCross-192-Frozen-Prenorm
-#SBATCH --nodelist=top
+#SBATCH --nodelist=aerosmith
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
